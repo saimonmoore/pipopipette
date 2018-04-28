1fc842bcb83795eb6d94ee6fdc3d7caca9ba1476
Saimon Moore <saimon.moore@xing.com>
2018-04-28 22:39:36 +0200

    Adding authentication
    TODO: Logout and cleanup + Signout button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,119 @@
 import React, { Component } from 'react'
 import { Provider, observer } from "mobx-react"
+import uniqid from "uniqid"
 
 import Store from './stores/Store.js'
 import Form from './components/Form.js'
 import Grid from './components/Grid.js'
 import logo from './logo.svg'
+import Auth from './lib/Auth.js'
+
 import './App.css'
 
 const store = new Store()
+const auth = new Auth()
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.initSession()
+  }
+
+  initSession() {
+    let session = this.session()
+
+    if (!session) {
+      this.signup((error) => {
+        if (error) {
+          alert(`Failed to signup! (${JSON.stringify(error)})`)
+        } else {
+          session = this.createSession()
+          this.login(session)
+        }
+      })
+    } else {
+      window.location.hash = session.session.id
+      this.login(session)
+    }
+  }
+
+  login(session) {
+    auth.login(this.getUserId(), (error, auth_user) => {
+      if (!error) {
+        Object.assign(session.user, { auth_user })
+        this.saveSession(session)
+      } else {
+        alert(`Failed to login! (${JSON.stringify(error)})`)
+      }
+    })
+  }
+
+  signup(fn) {
+    this.getUserId((signup, user_id) => {
+      if (signup) {
+        auth.signup(user_id, (error) => {
+          if (error) {
+            fn && fn(error, null)
+          } else {
+            fn && fn(null, user_id)
+          }
+        })
+      } else {
+        fn && fn(null, user_id)
+      }
+    })
+  }
+
+  saveSession(session) {
+    sessionStorage.setItem("session", JSON.stringify(session))
+  }
+
+  createSession() {
+    const session_id = this.session_id
+    const user_id = this.getUserId()
+
+    window.location.hash = session_id
+
+    const session = {
+      session: {
+        id: session_id
+      },
+      user: { user_id }
+    }
+
+    this.saveSession(session)
+    return session
+  }
+
+  get session_id() {
+    const hash = window.location.hash.substr(1)
+    return (hash.length && hash) || uniqid()
+  }
+
+  session() {
+    try {
+      return JSON.parse(sessionStorage.getItem("session"))
+    } catch(err) {
+      return false
+    }
+  }
+
+  getUserId(fn) {
+    const key = "pipopipette_user_id"
+    let user_id = localStorage.getItem(key)
+
+    if (!user_id) {
+      user_id = uniqid()
+      localStorage.setItem(key, user_id)
+      fn && fn(true, user_id)
+    } else {
+      fn && fn(false, user_id)
+    }
+
+    return user_id
+  }
 
   render() {
     const { grid_size, boxes, lines, dots } = store
---
+++ src/lib/Storage.js
@@ -0,0 +1,91 @@
+import * as firebase from "firebase";
+
+const APP = "pipopipette"
+
+class Storage {
+  constructor() {
+    this.initRemoteStorage();
+  }
+
+  initRemoteStorage() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(this.config())
+    }
+  }
+
+  config() {
+    return {
+      apiKey: "AIzaSyDo7kRspbEIVT202T5fuNf5ZTe1GTfZfD0",
+      authDomain: "pipo-pipette.firebaseapp.com",
+      databaseURL: "https://pipo-pipette.firebaseio.com",
+      projectId: "pipo-pipette",
+      storageBucket: "pipo-pipette.appspot.com",
+      messagingSenderId: "317436935763"
+    }
+  }
+
+  storage() {
+    return firebase.database();
+  }
+
+  // e.g. grid_size, colour
+  setUser(session_name, user) {
+    this.storage().ref(`${APP}/${session_name}/users/user_${user.id}`).set(user);
+  }
+
+  setDots(session_name, dots) {
+    this.storage().ref(`${APP}/${session_name}/dots`).set(dots);
+  }
+
+  setLines(session_name, lines) {
+    this.storage().ref(`${APP}/${session_name}/lines`).set(lines);
+  }
+
+  setBoxes(session_name, boxes) {
+    this.storage().ref(`${APP}/${session_name}/boxes`).set(boxes);
+  }
+
+  async getDots(session_name, fn) {
+    this.getArray(session_name, "dots", fn)
+  }
+
+  async getLines(session_name, fn) {
+    this.getArray(session_name, "lines", fn)
+  }
+
+  async getBoxes(session_name, fn) {
+    this.getArray(session_name, "boxes", fn)
+  }
+
+  async getArray(session_name, object, fn) {
+    let list = [];
+    let snapshot;
+    let error;
+
+    const ref = this.storage().ref(`${APP}/${session_name}/${object}`);
+    [error, snapshot] = await to(ref.once('value'));
+    if (error) return fn(error, null);
+
+    const response = Object.assign({}, snapshot.val());
+    list = response[object] || [];
+
+    fn(null, { list });
+  }
+
+  async getUser(session_name, user_id, fn) {
+    let user;
+    let snapshot;
+    let error;
+
+    const ref = this.storage().ref(`${APP}/${session_name}/users/user_${user_id}`);
+    [error, snapshot] = await to(ref.once('value'));
+    if (error) return fn(error, null);
+
+    const response = Object.assign({}, snapshot.val());
+    user = response.user
+
+    fn(null, { user });
+  }
+}
+
+export default Storage;
