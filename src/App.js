ec8850d502e133bff3bd82495e772b8e5d87b8dd
Saimon Moore <saimon.moore@xing.com>
2018-05-20 23:40:07 +0200

    Beautify recognition div

--- src/App.js
+++ src/App.js
@@ -209,6 +209,12 @@ class App extends Component {
             </div>
             <div className="RightPanel"></div>
           </div>
+          <div class="Recognition">
+            <div class="Author">Made by <a href="https://github.com/saimonmoore/pipopipette" title="Saimon Moore">Dr Moore</a></div>
+            <div class="Borrowings">
+              Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+            </div>
+          </div>
         </div>
       </Provider>
     );
