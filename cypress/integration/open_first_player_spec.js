bd238e5bfa26b162a445aef80b820d1a093520d0
Saimon Moore <saimon.moore@xing.com>
2018-05-29 15:00:22 +0200

    Add test for closed box

--- cypress/integration/open_first_player_spec.js
+++ cypress/integration/open_first_player_spec.js
@@ -1,97 +1,172 @@
 // TODO: Fix grid size to 3 x 3
 describe('When player 1 first loads app', function() {
-  const getStore = () => cy.window().its('app.store')
-  const getStorage = () => cy.window().its('app.storage')
+  const getStore = () => cy.window().its('app.store');
+  const getStorage = () => cy.window().its('app.storage');
 
   before(function() {
     cy.visit('/');
-    cy.hash({ timeout: 2000 }).then((hash) => {
-      Cypress.env("session_hash", hash)
+    cy.hash({timeout: 2000}).then(hash => {
+      Cypress.env('session_hash', hash);
 
       // Clear out existing testing sessions
-      getStorage().then((storage) => {
-        const currentSession = hash.slice(1)
-        console.log("clearing out old sessions except: ", currentSession)
+      getStorage().then(storage => {
+        const currentSession = hash.slice(1);
+        console.log('clearing out old sessions except: ', currentSession);
         storage.clearTestingSessions(currentSession, () => {
-          console.log("Old sessions deleted")
-        })
-      })
-    })
-  })
+          console.log('Old sessions deleted');
+        });
+      });
+    });
+  });
 
   it('shows Welcome dialogue', function() {
-    cy.contains('This is Pipopipette.')
-  })
+    cy.contains('This is Pipopipette.');
+  });
 
   it('redirects to url with session-specific hash', function() {
-    const session_hash = Cypress.env("session_hash")
-    expect(session_hash).to.match(/#\w{8}$/)
-  })
+    const session_hash = Cypress.env('session_hash');
+    expect(session_hash).to.match(/#\w{8}$/);
+  });
 
   context('when visiting page with session', function() {
-
     before(function() {
-      const session_hash = Cypress.env("session_hash")
-      cy.visit('/' + session_hash)
-    })
+      const session_hash = Cypress.env('session_hash');
+      cy.visit('/' + session_hash);
+    });
 
     it('shows Welcome dialogue', function() {
-      cy.contains('This is Pipopipette.')
-    })
+      cy.contains('This is Pipopipette.');
+    });
 
     context('when the other player joins', function() {
       before(function() {
         // Wait for the app to load...
-        cy.get('#root > div > header > div.Status > span').invoke('text').should('eq', 'Waiting for other player')
+        cy
+          .get('#root > div > header > div.Status > span')
+          .invoke('text')
+          .should('eq', 'Waiting for other player');
 
         getStore().then(store => {
-          store.handlePlayerAdded({ user_id: 1, colour: "green" })
-          cy.get('#root > div > header > div.Status > span').invoke('text').should('eq', 'Game started')
-        })
-      })
+          const colour = Cypress.env('player2_colour');
+          const user_id = Cypress.env('player2_id');
+
+          store.handlePlayerAdded({user_id, colour});
+          cy
+            .get('#root > div > header > div.Status > span')
+            .invoke('text')
+            .should('eq', 'Game started');
+        });
+      });
 
       it('hides Welcome dialogue', function() {
-        cy.get('.WaitingForPlayer').should('not.exist')
-      })
+        cy.get('.WaitingForPlayer').should('not.exist');
+      });
 
       it('status shows game started', function() {
-        cy.get('#root > div > header > div.Status > span').invoke('text').should('eq', 'Game started')
-      })
+        cy
+          .get('#root > div > header > div.Status > span')
+          .invoke('text')
+          .should('eq', 'Game started');
+      });
 
       it("shows player 1's turn", function() {
-        cy.contains('Your turn')
-      })
+        cy.contains('Your turn');
+      });
 
       it('shows score zero for player 1', function() {
-        cy.get('#root > div > header > div > div.LeftPanel > div > div.RightPanel > div.Score > span').invoke('text').should('eq', 'Score: 0');
-      })
+        cy
+          .get(
+            '#root > div > header > div > div.LeftPanel > div > div.RightPanel > div.Score > span',
+          )
+          .invoke('text')
+          .should('eq', 'Score: 0');
+      });
 
       it('shows score zero for player 2', function() {
-        cy.get('#root > div > header > div > div.RightPanel > div > div.RightPanel > div.Score > span').invoke('text').should('eq', 'Score: 0');
-      })
+        cy
+          .get(
+            '#root > div > header > div > div.RightPanel > div > div.RightPanel > div.Score > span',
+          )
+          .invoke('text')
+          .should('eq', 'Score: 0');
+      });
 
-      context('when player 1 plays', function() {
+      context('when player 1 plays 1 line', function() {
         before(function() {
-          cy.get('.Dot').first().click('left')
-          cy.get('.Dot').first().next().next().next().click('left')
+          cy.addsLine(0, 3);
         });
 
         it("shows player 2's turn", function() {
-          cy.contains('Their turn')
-        })
+          cy.contains('Their turn');
+        });
 
-        it("shows forbidden cursor", function() {
-          cy.get(".Dot").should(($dots) => {
-            $dots.each((i, $dot) => ( expect($dot).to.have.class("TheirTurn") ))
-          })
-        })
+        it('shows forbidden cursor', function() {
+          cy.get('.Dot').should($dots => {
+            $dots.each((i, $dot) => expect($dot).to.have.class('TheirTurn'));
+          });
+        });
 
-        it("shows 1 line", function() {
-          cy.get(".Line").should(($lines) => {
-            expect($lines).to.have.length(1)
-          })
-        })
+        it('shows 1 line', function() {
+          cy.get('.Line').should($lines => {
+            expect($lines).to.have.length(1);
+          });
+        });
+
+        it('shows score zero for player 1', function() {
+          cy
+            .get(
+              '#root > div > header > div > div.LeftPanel > div > div.RightPanel > div.Score > span',
+            )
+            .invoke('text')
+            .should('eq', 'Score: 0');
+        });
+
+        it('shows score zero for player 2', function() {
+          cy
+            .get(
+              '#root > div > header > div > div.RightPanel > div > div.RightPanel > div.Score > span',
+            )
+            .invoke('text')
+            .should('eq', 'Score: 0');
+        });
+      });
+
+      context('when player 1 closes a box', function() {
+        before(function() {
+          cy.otherAddsLine([1, 0], [1, 1]);
+          cy.addsLine(4, 1);
+          cy.otherAddsLine([1, 1], [2, 1]);
+          cy.addsLine(1, 0);
+        });
+
+        it("shows player 1's turn", function() {
+          cy.contains('Your turn');
+        });
+
+        it('shows 1 box closed', function() {
+          cy.get('.Box.Closed').should($boxes => {
+            expect($boxes).to.have.length(1);
+          });
+        });
+
+        it('shows score 1 for player 1', function() {
+          cy
+            .get(
+              '#root > div > header > div > div.LeftPanel > div > div.RightPanel > div.Score > span',
+            )
+            .invoke('text')
+            .should('eq', 'Score: 1');
+        });
+
+        it('shows score 0 for player 2', function() {
+          cy
+            .get(
+              '#root > div > header > div > div.RightPanel > div > div.RightPanel > div.Score > span',
+            )
+            .invoke('text')
+            .should('eq', 'Score: 0');
+        });
       });
     });
   });
-})
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,71 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const getStore = () => cy.window().its('app.store');
+
+/*
+ * Assuming a grid of dots like this:
+ *
+ * 0 3 6
+ * 1 4 7
+ * 2 5 8
+ *
+ * Usage:
+ *
+ *   cy.addLine(0, 3)
+ *
+ *   Will result in a line like this:
+ *
+ * 0-3 6
+ * 1 4 7
+ * 2 5 8
+ *
+ */
+Cypress.Commands.add('addsLine', (from, to) => {
+  debugger;
+  cy
+    .get('.Dot')
+    .eq(from)
+    .click('left');
+  cy
+    .get('.Dot')
+    .eq(to)
+    .click('left');
+});
+
+/*
+ * Assuming a grid of dots ( e.g. 3 x 3) with x/y axis like this:
+ *
+ * 0 1 2
+ * 1
+ * 2
+ *
+ * Usage:
+ *
+ *   cy.OtherAddsLine([0,0], [1,0])
+ *
+ *   where each argument represents columns and row coordinates:
+ *
+ *   from: [column, row]
+ *   to:   [column, row]
+ *
+ *   Will result in a line like this:
+ *
+ * 0-3 6
+ * 1 4 7
+ * 2 5 8
+ *
+ */
+Cypress.Commands.add('otherAddsLine', (from, to) => {
+  const colour = Cypress.env('player2_colour');
+  const user_id = Cypress.env('player2_id');
+  const player = {colour, user_id};
+  const fromDot = {column: from[0], row: from[1]};
+  const toDot = {column: to[0], row: to[1]};
+  const data = {from: fromDot, to: toDot, colour, player};
+
+  getStore().then(store => {
+    store.handleLineAdded(data);
+  });
+});
